fed89829f6e8cc1663897eadfb5cfc49a0c5c1a3
pablogonz <65257437+pablogonz@users.noreply.github.com>
2021-12-10 13:22:01 -0400

    nuget package temperatura

---
+++ demopac/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using temperaturekata;
+namespace demopac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double temperature;
+            Console.Write("Introduzca una temperatura:");
+            Console.WriteLine(" seleccione la escala  a la que quiere convertirla?");
+            Console.WriteLine(@"
+1- celcius to fahrenheit
+2- fahrenheit to celcius
+3-celcius to kelvin
+4-fahrenheit to kelvin");
+
+            int caso = Convert.ToInt32(Console.ReadLine());
+
+            switch (caso)
+            {
+                case 1:
+                    Console.WriteLine(temperaturekata.ctof(temperature));
+                    break;
+                case 2:
+                    Console.WriteLine(temperaturekata.ftoc(temperature));
+                    break;
+                case 3:
+                    Console.WriteLine(temperaturekata.ctok(temperature));
+                    break;
+                case 4:
+                    Console.WriteLine(temperaturekata.ftok(temperature));
+                    break;
+
+
+                default:
+                    break;
+            }
+        }
+    }
+}
